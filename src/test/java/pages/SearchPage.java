a3852d1b28d6970f3a2178f748bf5c16608d6572
coffee-anna <anna.motova.01@gmail.com>
2022-02-09 22:13:07 +0300

    Initial commit

---
+++ src/test/java/pages/SearchPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import tests.BaseTest;
+
+import java.io.UnsupportedEncodingException;
+
+public class SearchPage extends BaseTest {
+    WebDriver driver;
+    WebElement searchLine;
+    final String pageUrl = "https://google.com";
+    final String calcRequest = "калькулятор";
+
+    public SearchPage(WebDriver driver) {
+        this.driver = driver;
+        driver.get(pageUrl);
+    }
+
+    @Step("Redirecting to calculator page...")
+    public CalculatorPage goToCalc() {
+        searchLine = driver.findElement(By.name("q"));
+        searchLine.sendKeys(calcRequest, Keys.ENTER);
+        return new CalculatorPage(driver);
+    }
+}
---
+++ src/test/java/tests/SecondTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.io.UnsupportedEncodingException;
+
+import pages.CalculatorPage;
+import pages.SearchPage;
+
+@Epic("Calculator test")
+@Feature("Second case test")
+public class SecondTest extends BaseTest {
+    CalculatorPage calculator;
+    SearchPage search;
+    String problem;
+
+    @Test(description = "Testing calculator")
+    @Description("Opening Google Search, redirecting to calculator page and passing the math problem")
+    public void testCase(){
+        problem = "6/0";
+        search = new SearchPage(driver);
+        calculator = search.goToCalc();
+        calculator.inputProblem(problem);
+    }
+
+    @Test(description = "Verifying calculating result")
+    @Description("Comparing the expected math problem result with the actual")
+    public void verifyProblemResult() {
+        String answerField = calculator.getResultField();
+        Assert.assertEquals(answerField, "Infinity", "Unexpected result");
+    }
+
+    @Test(description = "Verifying calculator history field")
+    @Description("Comparing the expected calculator history with the actual")
+    public void verifyHistory() {
+        String historyField = calculator.getHistoryField();
+        String problemField = calculator.getProblemField();
+        Assert.assertEquals(historyField, problemField, "Unexpected history");
+    }
+}
